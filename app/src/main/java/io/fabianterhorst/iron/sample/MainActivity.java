896db32d0183b0155a8f001d7bea8b863e972c4a
fabianterhorst <fabian.terhorst@gmail.com>
2015-12-26 03:16:04 +0100

    update sample

--- app/src/main/java/io/fabianterhorst/iron/sample/MainActivity.java
+++ app/src/main/java/io/fabianterhorst/iron/sample/MainActivity.java
@@ -27,8 +27,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("Contributor size", MainStore.getContributors().size() + "");
-
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.github.com")
                 .addConverterFactory(MoshiConverterFactory.create())
