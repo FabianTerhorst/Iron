978a1016d5bfb26d6f0926f3a795b0f55b4fb537
fabianterhorst <fabian.terhorst@gmail.com>
2016-05-06 14:40:46 +0200

    fix kotlin test

--- iron/src/androidTest/kotlin/io/fabianterhorst/iron/KotlinCompatibilityTest.kt
+++ iron/src/androidTest/kotlin/io/fabianterhorst/iron/KotlinCompatibilityTest.kt
@@ -1,3 +1,15 @@
+package io.fabianterhorst.iron;
+
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.Before
+import org.junit.runner.RunWith
+
+import org.assertj.core.api.Assertions.assertThat
+
+import android.support.test.InstrumentationRegistry.getTargetContext
+
 @RunWith(AndroidJUnit4::class)
 class KotlinCompatibilityTest {
 
@@ -10,8 +22,8 @@ class KotlinCompatibilityTest {
 
     @Test
     fun testNormalClasses() {
-        class PersonNormalClass(val name: String = "name", val age: Int){
-            override fun equals(other: Any?): Boolean{
+        class PersonNormalClass(val name: String = "name", val age: Int) {
+            override fun equals(other: Any?): Boolean {
                 if (this === other) return true
                 if (other?.javaClass != javaClass) return false
 
@@ -23,7 +35,7 @@ class KotlinCompatibilityTest {
                 return true
             }
 
-            override fun hashCode(): Int{
+            override fun hashCode(): Int {
                 var result = name.hashCode()
                 result += 31 * result + age
                 return result
