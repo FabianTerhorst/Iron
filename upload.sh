2881a0a1c6231cc976bab6b6bf72356025790388
fabianterhorst <fabian.terhorst@gmail.com>
2016-06-22 12:21:03 +0200

    add upload script

---
+++ upload.sh
@@ -0,0 +1,5 @@
+./gradlew clean :iron:bintrayUpload
+./gradlew clean :iron-annotations:bintrayUpload
+./gradlew clean :iron-retrofit:bintrayUpload
+./gradlew clean :iron-compiler:bintrayUpload
+./gradlew clean :iron-encryption:bintrayUpload
